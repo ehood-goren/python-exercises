45f5b669326b7df75819a6da4f20755466479a9a
EhoodGoren <ehoodgoren@gmail.com>
2022-02-04 23:26:17 +0200

    Added function for writing all images to files
    Each src is format to begin with https first

--- 3. wikistealer/wikistealer.py
+++ 3. wikistealer/wikistealer.py
@@ -17,13 +17,13 @@ def wiki_search_word(word: str):
 def img_extractor(soup):
     """
     :param soup: soup to extract imgs from
-    :return: list of (alt, src) for each img in soup
+    :return: list of src for each img in soup
     """
     images = soup.find_all('img')
-    return [(img['alt'], img['src']) for img in images]
+    return [img['src'] for img in images]
 
 
-def create_word_dir(word):
+def create_word_dir(word: str):
     """
     :param word: the searched word
     :return: the dir path of the current search of the word
@@ -36,11 +36,40 @@ def create_word_dir(word):
     return images_path
 
 
+def check_https(url: str):
+    """
+    :param url: url to check
+    :return: url starting with https:// 
+    """
+    if url.startswith('https://'):
+        return url
+    if url.startswith('//'):
+        return f"https:{url}"
+    if url.startswith('/'):
+        return f"https://en.wikipedia.org{url}"
+    return f"https://{url}"
+
+
+def insert_images(images_src: list[str], path: str):
+    """
+    :param images_src: list of src of all images in page
+    :param path: path to write img files to
+    """
+    for img_src in images_src:
+        valid_src = check_https(img_src)
+        search_img = requests.get(valid_src)
+        img_content = search_img.content
+        img_name = img_src.split('/')[-1].replace('?','')
+        with open(f"{path}/{img_name}",'wb') as f:
+            f.write(img_content)
+                
+
 def wikistealer():
     input_word = input('Search a word: ')
     soup = wiki_search_word(input_word)
     images_info = img_extractor(soup)
-    create_word_dir(input_word)
+    path = create_word_dir(input_word)
+    insert_images(images_info, path)
 
 
 wikistealer()
